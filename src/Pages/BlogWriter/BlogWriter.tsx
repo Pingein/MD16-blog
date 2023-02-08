3ccb86c234ec6090ed4d13a513a70dbda4da0b63
Pingein <111181697+Pingein@users.noreply.github.com>
2023-02-09 01:22:00 +0200

    .

--- src/Pages/BlogWriter/BlogWriter.tsx
+++ src/Pages/BlogWriter/BlogWriter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import styles from './BlogWriter.module.scss'
 
 
@@ -10,30 +10,58 @@ const BlogWriter = ({}:BlogWriterParams) => {
 
     const [blogImage, setBlogImage] = useState('')
 
+    const titleRef = useRef<HTMLInputElement>(null)
+    const excerptRef = useRef<HTMLInputElement>(null)
+    const contentRef = useRef<HTMLTextAreaElement>(null)
+
     return (
         <section className={styles.fullscreenBlog}>
             <div className={styles.blog}>
                 <div className={styles.imageContainer}>
                     {blogImage 
-                    ? <img src={blogImage}></img> 
-                    : <form onSubmit={(e) => {
-                        e.preventDefault()
-                        let link = (e.currentTarget.childNodes[0] as HTMLInputElement).value
-                        if (link.match(RegExp(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/))) {
-                            setBlogImage(link)
-                        } else {
-                            alert('please enter valid url')
-                        }
-                      }}>
+                    ? <img src={blogImage} className={styles.image}></img> 
+                    : <form className={styles.blogContent} 
+                            onSubmit={(e) => {
+                                e.preventDefault()
+                                let link = (e.currentTarget.childNodes[0] as HTMLInputElement).value
+                                if (link.match(RegExp(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/))) {
+                                    setBlogImage(link)
+                                } else {
+                                    alert('please enter valid url')
+                                }
+                            }}>
                         <input type='text' 
-                               placeholder='Paste image link'/>
-                        <button>submit</button>
+                               placeholder='Paste image link'
+                               className={styles.input}/>
+                        <button className={styles.btn}>
+                            submit
+                        </button>
                       </form>}
                 </div>
-                <div className={styles.blogContent}>
-                    <h1 className={styles.blogTitle}>blog_title</h1>
-                    <p className={styles.blogText}>blog_text</p>
-                </div>
+                <form className={styles.blogContent}
+                      onSubmit={(e) => {
+                        e.preventDefault()
+                        if (titleRef && excerptRef && contentRef) {
+                            
+                        }
+                      }}>
+                    <input type="text" 
+                           placeholder='Enter blog title'
+                           className={styles.input}
+                           ref={titleRef}/>
+                    <input type="text" 
+                           placeholder='Enter blog excerpt'
+                           className={styles.input}
+                           ref={excerptRef}/>      
+                    <textarea placeholder='Enter blog text'
+                              className={styles.input}
+                              ref={contentRef}>
+                    </textarea>
+
+                    <button className={styles.btn}>
+                        Post
+                    </button>
+                </form>
             </div>
             
         </section>
